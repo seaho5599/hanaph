eae4081fd61509767acceacb3327fd2dc3b684b2
seaho5599 <seaho5599@gmail.com>
2022-07-01 18:07:24 +0900

    item스와이퍼play 버튼 js 작업

--- js/script.js
+++ js/script.js
@@ -10,4 +10,24 @@ window.onload = function() {
       clickable: true,
     },
   });
+  let slide_now = 'ing';
+
+  $('.sw-item-bt').click(function () {
+
+    if (slide_now == 'ing') {
+      // 현재 슬라이드 진행 중이라면
+      // 슬라이드를 멈춘다.
+      slide_now = 'stop';
+      sw_item.autoplay.stop();
+      $(this).find('span').text('chevron_right');
+
+    } else {
+      // 현재 슬라이드가 멈췄다.
+      // 다시 슬라이드를 진행한다.
+      slide_now = 'ing';
+      sw_item.autoplay.start();
+      $(this).find('span').text('pause');
+    }
+
+  });
 }
