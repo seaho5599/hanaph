59c1326bb1d0e2d2d0131446e910e74852aea83b
seaho5599 <seaho5599@gmail.com>
2022-07-05 18:09:06 +0900

    gnb submenu작업, play버튼 수정

--- js/script.js
+++ js/script.js
@@ -10,24 +10,17 @@ window.onload = function() {
       clickable: true,
     },
   });
-  let slide_now = 'ing';
-
-  $('.sw-item-bt').click(function () {
-
-    if (slide_now == 'ing') {
-      // 현재 슬라이드 진행 중이라면
-      // 슬라이드를 멈춘다.
-      slide_now = 'stop';
-      sw_item.autoplay.stop();
-      $(this).find('span').text('chevron_right');
-
-    } else {
-      // 현재 슬라이드가 멈췄다.
-      // 다시 슬라이드를 진행한다.
-      slide_now = 'ing';
+  let swItemBt = $('.sw-item-bt');
+  swItemBt.click(function(){
+    let temp = $(this).hasClass('sw-item-bt-play');
+    if(temp == true){
       sw_item.autoplay.start();
-      $(this).find('span').text('pause');
+      $(this).removeClass('sw-item-bt-play');
+    }else{
+      sw_item.autoplay.stop();
+      $(this).addClass('sw-item-bt-play');
     }
-
   });
+
+  
 }
